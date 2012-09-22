41895558d9d670229cb1ab3173502a6541b9c8cb
Marco Vinicio Artavia Quesada <marcovx@MarcoVX-LaptopUb.(none)>
2012-09-21 18:28:11 -0600

    Primer commit

---
+++ TanksVsZombies/gen/cr/ac/ucr/ecci/ci2354/TanksvsZombies/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cr.ac.ucr.ecci.ci2354.TanksvsZombies;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TanksVsZombies/src/cr/ac/ucr/ecci/tanksvszombies/MainActivity.java
@@ -0,0 +1,21 @@
+package cr.ac.ucr.ecci.tanksvszombies;
+
+import cr.ac.ucr.ecci.ci2354.TanksvsZombies.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
