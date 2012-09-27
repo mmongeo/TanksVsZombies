e50b7961d891448800c1508f71100ef0c2506267
Ruben <rubenjg1991@hotmail.com>
2012-09-26 18:47:13 -0600

    Wireframes de GameActivity y GameStore listos

--- TanksVsZombies/gen/cr/ac/ucr/ecci/ci2354/TanksvsZombies/R.java
+++ TanksVsZombies/gen/cr/ac/ucr/ecci/ci2354/TanksvsZombies/R.java
@@ -10,34 +10,65 @@ package cr.ac.ucr.ecci.ci2354.TanksvsZombies;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int game_activity_background=0x7f040000;
+        public static final int game_activity_high_score=0x7f040001;
+        public static final int game_activity_high_score_shadow=0x7f040002;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int links=0x7f020002;
+        public static final int shield=0x7f020003;
+        public static final int store_item_default=0x7f020004;
+        public static final int store_item_default_mini=0x7f020005;
+        public static final int tank=0x7f020006;
+        public static final int zombie=0x7f020007;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int game_store_detail_image=0x7f08000b;
+        public static final int game_store_detail_item_description=0x7f08000d;
+        public static final int game_store_detail_object_name=0x7f08000a;
+        public static final int game_store_detail_object_price=0x7f08000c;
+        public static final int game_store_list_item_image=0x7f080007;
+        public static final int game_store_list_item_object_name=0x7f080008;
+        public static final int game_store_list_item_price=0x7f080009;
+        public static final int menu_settings=0x7f08000e;
+        public static final int shield_1=0x7f080000;
+        public static final int shield_2=0x7f080001;
+        public static final int shield_3=0x7f080002;
+        public static final int storeitem_detail_container=0x7f080005;
+        public static final int storeitem_list=0x7f080006;
+        public static final int zombie_1=0x7f080003;
+        public static final int zombie_2=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_storeitem_detail=0x7f030001;
+        public static final int activity_storeitem_list=0x7f030002;
+        public static final int activity_storeitem_list_single_item=0x7f030003;
+        public static final int activity_storeitem_twopane=0x7f030004;
+        public static final int fragment_storeitem_detail=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_game=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int highScores=0x7f040008;
-        public static final int mainMenu=0x7f04000b;
-        public static final int menu_settings=0x7f040002;
-        public static final int newGame=0x7f040009;
-        public static final int store=0x7f04000a;
-        public static final int title_activity_main=0x7f040003;
-        public static final int title_activity_main_menu=0x7f040007;
-        public static final int title_section1=0x7f040006;
-        public static final int title_section2=0x7f040005;
-        public static final int title_section3=0x7f040004;
+        public static final int app_name=0x7f050000;
+        public static final int game_activity_score_text=0x7f050006;
+        public static final int game_activity_shield_image_description=0x7f050004;
+        public static final int game_activity_tank_image_description=0x7f050003;
+        public static final int game_activity_zombie_image_description=0x7f050005;
+        public static final int lorem_ipsum=0x7f05000c;
+        public static final int menu_settings=0x7f050001;
+        public static final int store_activity_default_price=0x7f050009;
+        public static final int store_activity_default_text=0x7f050008;
+        public static final int store_activity_no_image_description=0x7f050007;
+        public static final int title_activity_game=0x7f050002;
+        public static final int title_storeitem_detail=0x7f05000a;
+        public static final int title_storeitem_list=0x7f05000b;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
