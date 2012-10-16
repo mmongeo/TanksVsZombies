0d56f00f6b3819fe8375c9b8ce267a2219841974
mario <mario@PHENOM.(none)>
2012-10-16 14:57:31 -0600

    pobando andengine

--- TanksVsZombies/src/cr/ac/ucr/ecci/ci2354/TanksvsZombies/ui/MainMenuFragment.java
+++ TanksVsZombies/src/cr/ac/ucr/ecci/ci2354/TanksvsZombies/ui/MainMenuFragment.java
@@ -1,13 +1,17 @@
 package cr.ac.ucr.ecci.ci2354.TanksvsZombies.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 import cr.ac.ucr.ecci.ci2354.TanksvsZombies.R;
+import cr.ac.ucr.ecci.ci2354.TanksvsZombies.game.GameActivity;
 
 public class MainMenuFragment extends SherlockFragment {
 
@@ -19,7 +23,18 @@ public class MainMenuFragment extends SherlockFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
-    	return inflater.inflate(R.layout.fragment_main_menu, null);
+    	View vista = inflater.inflate(R.layout.fragment_main_menu, null);
+        Button boton = (Button) vista.findViewById(R.id.boton_iniciarJuego);
+        boton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(),GameActivity.class));
+				getActivity().finish(); //no se si hace falta
+			}
+		});
+    	return vista;
     }
+   
     
 }
