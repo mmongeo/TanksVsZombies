1934eda409c30fe39ee82623bdebadda97069dd3
RubenJG <rubenjg1991@gmail.com>
2012-11-14 11:23:54 -0600

    Temp

--- TanksVsZombies/src/cr/ac/ucr/ecci/ci2354/TanksvsZombies/game/PauseableTimerHandler.java
+++ TanksVsZombies/src/cr/ac/ucr/ecci/ci2354/TanksvsZombies/game/PauseableTimerHandler.java
@@ -2,8 +2,6 @@ package cr.ac.ucr.ecci.ci2354.TanksvsZombies.game;
 
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
-
-import android.util.Log;
  /*
   * http://bhamdevelopment.blogspot.com/2012/08/andengine-extensions-pausable-timer.html
   * Este codigo fue copiado del link anterior.
